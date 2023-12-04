9cc3281bb4b8c3e610a538124afe97cf1f93faae
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2023-12-03 22:04:35 -0500

    Segundo commit: Ingreso de datos de la funcion principal

--- Suma_funcion.c
+++ Suma_funcion.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <tima.h>
+#include <time.h>
 
 void suma(int a, int b, int matrizA[a][b], int matrizB[a][b], int matrizR[a][b])
 {
@@ -11,4 +11,23 @@ void suma(int a, int b, int matrizA[a][b], int matrizB[a][b], int matrizR[a][b])
             matrizR[i][j] = matrizA[i][j] + matrizB[i][j];
         }
     }
+}
+
+
+int main () {
+
+    int a, b;
+    int matrizA[a][b];
+    int matrizB[a][b];
+    int matrizR[a][b];
+
+    srand(time(NULL));
+
+    printf ("Ingrese el valor de las filas: ");
+    scanf ("%d", &a);
+
+    printf ("Ingrese el valor de las columnas: ");
+    scanf ("%d", &b);
+    
+    return 0;
 }
